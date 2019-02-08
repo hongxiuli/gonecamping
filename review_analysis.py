2a0435d32d7fbadaca11df4166191cd4fce78a49
Hongxiu Li <leexiux@gmail.com>
2019-02-07 23:28:01 -0500

    analyze user reviews

---
+++ review_analysis.py
@@ -0,0 +1,42 @@
+import os
+import pandas as pd
+import math
+
+def process_user_review():
+    files = os.listdir("./reviews")
+    names = ['user'] + [f.replace('.csv', '') for f in files]
+    df = pd.DataFrame(columns=names)
+    df.set_index("user", inplace = True)
+    count=1
+    for f in files:
+        print("processing %s, %d out of %d" %(f, count, len(files)))
+        temp = pd.read_csv('./reviews/'+f, encoding='utf-8')
+        users = df.index
+        for index, row in temp.iterrows():
+            #if(row['user'] in users):
+            #    print('%s has more than 1 comment' % row['user'])
+            df.at[row['user'], row['name']] = row['stars']
+        count += 1
+        if(count%10==0):
+            df.to_csv('public_campsites_user_reviews.csv', encoding='utf-8', index=True, header=True)
+    print('writing to csv')
+    df.to_csv('public_campsites_user_reviews.csv', encoding='utf-8', index=True, header=True)
+
+def remove_user_with_one_review():
+    df = pd.read_csv('public_campsites_user_reviews.csv', encoding='utf-8')
+    columns = df.columns.tolist()
+    columns.remove('user')
+    indexes_to_keep = []
+    for index, row in df.iterrows():
+        good = 0
+        for c in columns:
+            if(not math.isnan(row[c])):
+                good +=1
+                if(good > 1):
+                    indexes_to_keep.append(index)
+                    break
+    
+    df = df.iloc[indexes_to_keep]
+    df.to_csv('public_campsites_user_reviews_more_than_one.csv', encoding='utf-8', index=False, header=True)
+
+remove_user_with_one_review()
